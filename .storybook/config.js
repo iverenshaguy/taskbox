71a39f12af317d96366b36c0b2ce95ed132f9b1d
Iveren Shaguy <iverenshaguy@gmail.com>
2019-08-25 23:08:25 +0100

    taskbox UI

---
+++ .storybook/config.js
@@ -0,0 +1,16 @@
+import { configure } from '@storybook/react';
+import requireContext from 'require-context.macro';
+
+import '../src/index.css';
+
+// function loadStories() {
+//   require('../src/stories');
+// }
+
+const req = requireContext('../src/components', true, /\.stories\.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
---
+++ src/components/InboxScreen .stories.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { Provider } from 'react-redux';
+
+import { PureInboxScreen } from './InboxScreen';
+import { defaultTasks } from './TaskList.stories';
+
+const store = {
+  getState: () => {
+    return {
+      tasks: defaultTasks,
+    };
+  },
+  subscribe: () => 0,
+  dispatch: action('dispatch'),
+};
+
+storiesOf('InboxScreen', module)
+  .addDecorator(story => <Provider store={store}>{story()}</Provider>)
+  .add('default', () => <PureInboxScreen />)
+  .add('error', () => <PureInboxScreen error="Something" />);
